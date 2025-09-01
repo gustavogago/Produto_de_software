95b84eda8f7c570f03e3cc2877c91c4df7612eeb
Dimitri <dimitri.s.amado@gmail.com>
2025-09-01 16:15:32 -0300

    feat: add metadata to CreateUserView for improved clarity

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,6 +6,9 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class CreateUserView(generics.CreateAPIView):
+    name = "Cadastro de Usuário"
+    http_method_names = ["post"]
+    description = "Endpoint for creating a new user."
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
