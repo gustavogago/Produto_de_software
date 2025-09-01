1eb90da9521676cc2db6a5448026eda9c910a5be
Dimitri <dimitri.s.amado@gmail.com>
2025-09-01 16:15:15 -0300

    feat: update UserSerializer to include first and last name fields

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,12 +5,14 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password"]
+        fields = ["id", "first_name", "last_name", "email", "password"]
         extra_kwargs = {
             "password": {"write_only": True},
             "email": {"required": True},
         }
 
     def create(self, validated_data):
+        email = validated_data.get("email")
+        validated_data["username"] = email
         user = User.objects.create_user(**validated_data)
         return user
