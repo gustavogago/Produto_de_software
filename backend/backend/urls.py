50ab509f1dc38a926e46e146852bd5fca9ca0bc8
Dimitri <dimitri.s.amado@gmail.com>
2025-09-04 11:30:54 -0300

    fix: Change the login route name to follow the pattern

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/register/", CreateUserView.as_view(), name="register"),
-    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/users/login/", TokenObtainPairView.as_view(), name="login"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
 ]
