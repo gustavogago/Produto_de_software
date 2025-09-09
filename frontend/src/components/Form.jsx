8a12eb8851ac78272c0814d9371c10e7c555019c
Felipe Souza <felipessouza382@gmail.com>
2025-09-09 16:34:03 -0300

    correção frontend

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,26 +1,27 @@
-import { useState } from "react"
-import api from "../api"
-import { useNavigate } from "react-router-dom"
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css"
+//import LoadingIndicator from "./LoadingIndicator";
 
-function Form({ route, method}){
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+function Form({ route, method }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-    const name = method === "login" ? "Login" : "Register"
+    const name = method === "login" ? "Login" : "Register";
 
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
-        
-        try{
-            const res = await api.post(route, {username, password})
-            if(method === "login"){
-                localStorage.setItem(ACCESS_TOKEN, res.data.access)
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
+
+        try {
+            const res = await api.post(route, { username, password })
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
                 navigate("/")
             } else {
                 navigate("/login")
@@ -29,31 +30,31 @@ function Form({ route, method}){
             alert(error)
         } finally {
             setLoading(false)
-        }    
-    }
+        }
+    };
 
-    return <form onSubmit={handleSubmit} className="form-contaneir">
-        <h1>{name}</h1>
-        <imput 
-            className="form-input"
-            typer="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-        />
-
-        <imput 
-            className="form-input"
-            typer="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-        />
-
-        <button className="form-button" type="submit">
-            {name}
-        </button>
-    </form>
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{name}</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            <button className="form-button" type="submit">
+                {name}
+            </button>
+        </form>
+    );
 }
 
 export default Form
