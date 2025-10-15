59f787a3839da958c6bfae29959d98283af8e924
Dimitri <dimitri.s.amado@gmail.com>
2025-10-15 20:25:57 -0300

    fix: Right url name item -> items to follow the pattern

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+
 from . import views
-from .views import UserProfileView, UserProfileUpdateView 
+from .views import UserProfileUpdateView, UserProfileView
 
 urlpatterns = [
     path("items/", views.CreateItemView.as_view(), name="note-list"),
     path("items/delete/<int:pk>/", views.DeleteItemView.as_view(), name="delete-note"),
-    path("item/update/<int:pk>/", views.UpdateItemView.as_view(), name="update-note"),
+    path("items/update/<int:pk>/", views.UpdateItemView.as_view(), name="update-note"),
     path("items/<int:pk>/", views.ReadItemView.as_view(), name="note-detail"),
     path("profile/", UserProfileView.as_view(), name="user-profile"),
-    path("profile/update/", UserProfileUpdateView.as_view(), name="user-update")
+    path("profile/update/", UserProfileUpdateView.as_view(), name="user-update"),
 ]
