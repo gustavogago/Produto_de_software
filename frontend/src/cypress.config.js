0a8a53770b4d748e1d1ba60c58b0f4507e024e45
PauloVPorto <paulo.v@gec.inatel.br>
2025-10-20 10:19:43 -0300

    test prod

--- frontend/src/cypress.config.js
+++ frontend/src/cypress.config.js
@@ -2,12 +2,33 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    },
     baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:5173",
     env: {
       apiUrl: process.env.CYPRESS_API_URL || "http://127.0.0.1:8000",
-      loginPath: "/login"
+      loginPath: "/login",
     },
-    video: false,
     chromeWebSecurity: false,
+    
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
   },
+
+  
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    reportPageTitle: "Relatório E2E",
+    embeddedScreenshots: true,
+    inlineAssets: true,      // gera um HTML auto-contido
+    saveAllAttempts: false,
+  },
+
+  
+  video: false,
+  screenshotOnRunFailure: true,
 });
