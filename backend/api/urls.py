6f6ee569ad48675ee0ba030ba54322d4323437be
Dimitri <dimitri.s.amado@gmail.com>
2025-10-21 01:38:56 -0300

    refactor: Padronizando as URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 
 from . import views
-from .views import UserProfileUpdateView, UserProfileView
+from .views import (
+    ListCategoriesView,
+    ListCitiesView,
+    UserProfileUpdateView,
+    UserProfileView,
+)
 
 urlpatterns = [
-    path("items/", views.CreateItemView.as_view(), name="note-list"),
+    path("users/profile/", UserProfileView.as_view(), name="user-profile"),
+    path("users/profile/update/", UserProfileUpdateView.as_view(), name="user-update"),
+    path("items/create/", views.CreateItemView.as_view(), name="note-list"),
     path("items/delete/<int:pk>/", views.DeleteItemView.as_view(), name="delete-note"),
     path("items/update/<int:pk>/", views.UpdateItemView.as_view(), name="update-note"),
     path("items/<int:pk>/", views.ReadItemView.as_view(), name="note-detail"),
-    path("profile/", UserProfileView.as_view(), name="user-profile"),
-    path("profile/update/", UserProfileUpdateView.as_view(), name="user-update"),
+    path("categories/", ListCategoriesView.as_view(), name="list-categories"),
+    path("cities/", ListCitiesView.as_view(), name="list-cities"),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,21 @@
+from api.views import CreateUserView
 from django.contrib import admin
+<<<<<<< HEAD
 from django.urls import path, include
 from api.views import CreateUserView
+=======
+from django.urls import include, path
+>>>>>>> 1aa3594 (refactor: Padronizando as URLs)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 #Faz a configuração das rotas a partir das URLS
 urlpatterns = [
     path("admin/", admin.site.urls),
     #path("api/health/", health_check, name='health_check'),  # Adicione esta linha
-    path("api/users/register/", CreateUserView.as_view(), name="register"),
-    path("api/users/login/", TokenObtainPairView.as_view(), name="login"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("users/", include("api.urls")),
+    path("users/register/", CreateUserView.as_view(), name="register"),
+    path("users/login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/users/", include("api.urls")),
-    path("items/", include("api.urls")),  
+    path("", include("api.urls")),  
 ]
