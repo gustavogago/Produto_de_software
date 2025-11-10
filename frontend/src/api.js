71895da21befc04cd114b8d6e74523e1c2325069
Dimitri <dimitri.s.amado@gmail.com>
2025-11-10 13:46:10 -0300

    feat: Update API base URL configuration to use environment variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use(
