8414447c5d54b2b154828844f56a5bd50cad9d9f
🐄1️⃣ <161668608+KBritoR@users.noreply.github.com>
2025-11-11 22:43:37 -0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,7 @@
 import os
 from datetime import timedelta
 from pathlib import Path
+import sys
 
 from dotenv import load_dotenv
 
@@ -118,3 +119,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+if 'test' in sys.argv:
+    DATABASES['default']['OPTIONS'] = {'sslmode': 'disable'}
